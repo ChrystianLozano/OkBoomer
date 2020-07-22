e52592447390a14973aa93bf1570854a49280649
Chrystian Lozano <chrystianlozano@icloud.com>
2020-07-22 18:14:32 -0500

    Variables y tipo de datos

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+$lastname = 'Lozano';
+$name = "Chrystian $lastname";
+var_dump($name); //saber el tipo de datos de un avariable
+?>
 <!doctype html>
 <html lang="en">
 
@@ -21,7 +26,7 @@
         <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
       </div>
       <div class="col">
-        <h1><?php echo 'Chrystian Lozano'; ?></h1>
+        <h1><?php echo $name; ?></h1>
         <h2>PHP Developer</h2>
         <ul>
           <li>Mail: hector@mail.com</li>
