62a383441b933691e7aeabc8a90030d1d853c008
Chrystian Lozano <chrystianlozano@icloud.com>
2020-07-23 18:56:35 -0500

    Constructor y Métodos

--- jobs.php
+++ jobs.php
@@ -6,32 +6,50 @@ class Job {
   public $visible;
   public $months;
 
-  public function setTitle($title){
-    $this->title = $title;
+
+  public function __construct($t, $d) {
+    $this->setTitle($t);
+    $this->description = $d;
+  }
+
+
+  public function setTitle($t){
+    if($t == ''){
+      $this->title = 'N/A';
+    }else{
+    $this->title = $t;
+    }
   }
 
   public function getTitle() {
     return $this->title;
   }
+
+  public function getDurationAsString(){
+    $years = floor($this->months / 12);
+    $extraMonths = $this->months % 12;
+    return "$years años $extraMonths meses";
+  }
 }
 
-$job1 = new Job();
-$job1->setTitle('PHP Developer');
-$job1->description = 'esto es una descripcion';
+$job1 = new Job('PHP Developer', 'esto es una descripcion');
 $job1->visible = true;
 $job1->months = 5;
 
-$job2 = new Job();
-$job2->setTitle(WebMaster);
-$job2->description = 'lo perron de lo perron';
+$job2 = new Job('WebMaster', 'lo perron de lo perron');
 $job2->visible = true;
 $job2->months = 7;
+
+$job3 = new Job('', 'lo perron de lo perron');
+$job3->visible = true;
+$job3->months = 2;
 
 
 
 $jobs = [
     $job1,
     $job2,
+    $job3,
     // [
     //   'title' => 'PHP Developer',
     //   'description' => 'esto es una descripcion',
@@ -68,11 +86,7 @@ $jobs = [
   
   ];
 
-  function getDuration($months){
-    $years = floor($months / 12);
-    $extraMonths = $months % 12;
-    return "$years años $extraMonths meses";
-  }
+ 
   
   function printJob($job){
     if($job->visible == false){
@@ -83,7 +97,7 @@ $jobs = [
     echo  '<li class="work-position">';
     echo  '<h5>' . $job->getTitle() . '</h5>';
     echo  '<p>' . $job->description . '</p>';
-    echo  '<p>' . getDuration($job->months) . '</p>';
+    echo  '<p>' . $job->getDurationAsString() . '</p>';
   
     echo  '<strong>Achievements:</strong>
       <ul>
